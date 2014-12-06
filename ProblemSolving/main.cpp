317bce1d4c7831cacc117a7012631c2fafc06075
Jonghwa Woo <wjh0630@gmail.com>
2014-12-06 23:09:32 +0900

    Compress main.cpp (?)

--- ProblemSolving/main.cpp
+++ ProblemSolving/main.cpp
@@ -1,23 +1,19 @@
 #include <iostream>
 #include <cstring>
+#include <string>
 #include <algorithm>
 #include <vector>
 #include <queue>
 #include <stack>
 #include <functional>
 #include <numeric>
+#include <limits>
 
 using namespace std;
 
 int n;
 
-int main()
+int main() { ios::sync_with_stdio(false); int tests; cin >> tests; while (tests--)
 {
-    int tests;
-
-    cin >> tests;
-
-    while (tests--)
-    {
-    }
-}
+    cin >> n;
+}}
