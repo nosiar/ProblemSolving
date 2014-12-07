0656c6c5245562eb88243e7b35391fbbe722cb0f
Jonghwa Woo <wjh0630@gmail.com>
2014-12-08 00:45:14 +0900

    Add geometry

---
+++ ProblemSolving/src/geometry.cpp
@@ -0,0 +1,37 @@
+﻿#include <cmath>
+#include <utility>
+
+#define PI 3.14159265358979323846
+#define EPS 1e-9
+
+#define x first
+#define y second
+
+typedef std::pair<double, double> point;
+typedef std::pair<double, double> vec;
+
+double length(vec v)
+{
+    return std::sqrt(v.x*v.x + v.y*v.y);
+}
+
+vec normalize(vec v)
+{
+    double len = length(v);
+    return{ v.x / len, v.y / len };
+}
+
+double dot(vec v0, vec v1)
+{
+    return v0.x*v1.x + v0.y*v1.y;
+}
+
+vec operator+(vec v0, vec v1)
+{
+    return{ v0.x + v1.x, v0.y + v1.y };
+}
+
+vec operator-(vec v0, vec v1)
+{
+    return{ v0.x - v1.x, v0.y - v1.y };
+}
