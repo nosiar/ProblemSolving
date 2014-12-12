fa31d33fc45800d955a7975c050799de6bff1679
Jonghwa Woo <wjh0630@gmail.com>
2014-12-12 20:59:00 +0900

    Add Floyd–Warshall algorithm

--- ProblemSolving/src/graph.cpp
+++ ProblemSolving/src/graph.cpp
@@ -3,32 +3,32 @@
 #include <functional>
 
 /************************************************************************/
-/*  e[from] : vector of {weight, to}                                    */
-/*  n : number of vertex                                                */
+/*  edges[from] : vector of {weight, to}                                    */
+/*  v : number of vertex                                                */
 /*  s : source                                                          */
 /*  d : destination                                                     */
 /************************************************************************/
 template<class T>
-T dijkstra(std::vector<std::pair<T, int>> *e, int n, int s, int d)
+T dijkstra(std::vector<std::pair<T, int>> *edges, int v, int s, int d)
 {
     std::priority_queue<std::pair<T, int>,
                         std::vector<pair<T, int>>, 
                         std::greater<pair<T, int>>> q;
     q.push({ 0.0, s });
 
-    std::vector<T> dist(n, 987654321.0);
+    std::vector<T> dist(v, 987654321.0);
     dist[s] = 0;
 
     while (!q.empty())
     {
-        auto cur = q.top(); q.pop();
+        auto here = q.top(); q.pop();
 
-        if (cur.first > dist[cur.second])
+        if (here.first > dist[here.second])
             continue;
 
-        for (auto edge : e[cur.second])
+        for (auto edge : edges[here.second])
         {
-            T new_dist = dist[cur.second] + edge.first;
+            T new_dist = dist[here.second] + edge.first;
             if (dist[edge.second] > new_dist)
             {
                 dist[edge.second] = new_dist;
@@ -38,4 +38,34 @@ T dijkstra(std::vector<std::pair<T, int>> *e, int n, int s, int d)
     }
 
     return dist[d];
+}
+
+template<class T>
+void floyd_warshall(std::vector<std::pair<T, int>> *edges, int v, T* dist[])
+{
+    for (int i = 0; i < v; ++i)
+    {
+        for (int j = 0; j < v; ++j)
+        {
+            if (j == i)
+                dist[i][j] = 0;
+            else
+                dist[i][j] = 987654321;
+        }
+        for (auto edge : edges[i])
+            dist[i][edge.second] = edge.first;
+    }
+
+    for (int k = 0; k < v; ++k)
+    {
+        for (int i = 0; i < v; ++i)
+        {
+            for (int j = 0; j < v; ++j)
+            {
+                T new_dist = dist[i][k] + dist[k][j];
+                if (dist[i][j] > new_dist)
+                    dist[i][j] = new_dist;
+            }
+        }
+    }
 }
