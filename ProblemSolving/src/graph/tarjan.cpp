851aef5b32e13feca20281d0bc7cc2f1b2ae42fa
Jonghwa Woo <wjh0630@gmail.com>
2015-03-29 17:49:16 +0900

    Add Tarjan's strongly connected components algorithm

---
+++ ProblemSolving/src/graph/tarjan.cpp
@@ -0,0 +1,66 @@
+#include <vector>
+#include <stack>
+#include <algorithm>
+
+struct tarjan
+{
+    int n;
+    std::vector<std::vector<int>>& adj;
+    std::vector<bool> finished;
+    std::vector<int> id;
+    std::vector<int> scc_id;
+    int next_id;
+    int next_scc_id;
+    std::stack<int> st;
+
+    tarjan(std::vector<std::vector<int>>& adj_) : adj(adj_), n(adj_.size()), finished(n), id(n), next_id(0), next_scc_id(0)
+    {
+    }
+
+    int scc(int i)
+    {
+        st.push(i);
+
+        int ret = id[i] = next_id++;
+
+        for (int j : adj[i])
+        {
+            if (id[j] == -1)
+                ret = std::min(ret, scc(j));
+            else if (!finished[j])
+                ret = std::min(ret, id[j]);
+        }
+
+        if (id[i] == ret)
+        {
+            while (true)
+            {
+                int x = st.top(); st.pop();
+                scc_id[x] = next_scc_id;
+
+                if (x == i)
+                    break;
+            }
+            ++next_scc_id;
+        }
+
+        finished[i] = true;
+
+        return ret;
+    }
+
+    void do_tarjan()
+    {
+        for (int i = 0; i < n; ++i)
+            if (id[i] == -1)
+                scc(i);
+    }
+
+    const std::vector<int>& operator()()
+    {
+        if (!finished[0])
+            do_tarjan();
+
+        return scc_id;
+    }
+};
